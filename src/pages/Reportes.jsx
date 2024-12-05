76b3004a6abb8a2c215ca37c758da61b3fde3b07
Neroj66 <jerickacosta66@gmail.com>
2024-12-05 17:03:15 -0500

    actualizacion bi

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 const Reportes = () => {
   return (<Container>
-<iframe title="MÓDULO DE PEDIDOS - ESTADISTICA" width="1400" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiNDkxMDAyOGUtNTFhYi00NGY0LWE3ZDItOWM2MmE2NWZlOTE3IiwidCI6ImQxNjNlOWUwLTBhZTAtNDRlZi1iYWMxLWFkNTAyYzgwMzAxNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="MÓDULO DE PEDIDOS - ESTADISTICA" width="1400" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiYzg5NjA4MmEtMTU0ZC00MzQ2LWE0OGQtYWQ5MmY1ZTQxMTZmIiwidCI6ImQxNjNlOWUwLTBhZTAtNDRlZi1iYWMxLWFkNTAyYzgwMzAxNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
   </Container>);
 }
 const Container =styled.div`

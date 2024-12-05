78df51a7f132ebbe6677d7141602db917367dcc3
Neroj66 <jerickacosta66@gmail.com>
2024-12-05 11:13:22 -0500

    agregando reporte bi

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 const Reportes = () => {
   return (<Container>
-<h1>En proceso...</h1>
+<iframe title="MÓDULO DE PEDIDOS - ESTADISTICA" width="1400" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiNDkxMDAyOGUtNTFhYi00NGY0LWE3ZDItOWM2MmE2NWZlOTE3IiwidCI6ImQxNjNlOWUwLTBhZTAtNDRlZi1iYWMxLWFkNTAyYzgwMzAxNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
   </Container>);
 }
 const Container =styled.div`
